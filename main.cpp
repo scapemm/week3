1369e0a80ceffd046429fdfe8db6ff8fba6fc27c
scapemm <a.shukurullayev@newuu.uz>
2024-09-13 12:34:02 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+    // problem 9
+//    double a,b,c;
+//    cout << "Enter v.0 :";cin >> a;
+//    cout << "Enter v.0: "; cin >> b;
+//    cout << "Enter time: "; cin >> c;
+//
+
+//       cout << (b-a)/c << "\n";
+
+       // problem 10
+//       double dis,eff,pri;
+//       cout << "Enter distance to drive :"; cin >> dis;
+//       cout << "Enter fuel efficciency: "; cin >> eff;
+//       cout << "Enter price: ";cin >> pri;
+//       cout << dis/eff*pri << "\n";
+
+
+           // problem 11
+
+
+//    double a=1.13e3,b=411e-4;
+//    cout << "Example Values 1.533e4 and 424e-3\n";
+//    cout << a << ' ' << b << "\n";
+    // problem 12
+//    cout << 241/11 << ' ';
+//    if(241%11==0){
+//        cout << 11 << "\n";
+//    }
+//    else cout << 241%11 << "\n";
+
+    // problem 13
+//    double s;
+//    cin >> s;
+//    cout <<  "The area of the hexagon is: " << sqrt(27) / 2 * s *s << "\n";
+    // problem 14
+//    char a = 'A'; // HELLO 65
+//    cout <<  char(int(a)+7) ;
+//    cout << char(int(a)+4) ;
+//    cout << char(int(a)+11) ;
+//    cout << char(int(a)+11) ;
+//    cout << char(int(a)+14) ;
+//    cout << char(int(a)-32);
+
+    // problem 15
+//    int x=17,y=1;
+//    double z = (x + ++x) / ((++(++y)) * double((--y)));
+//    cout << z << "\n";
+
+    // problem 16
+
+//    int a,b;
+//    cin >> a >> b;
+//    cout << (a==b);
+
+
+    // problem 17
+//    int a,b;
+//    cin >> a >> b;
+//    cout << ((a<140) && (a<b)) << "\n";
+
+
+    // problem 18
+//    int a,b,c;
+//    cin >> a >> b >> c;
+//    cout << (a>=b && a>=c);
+
+
+    // problem 19
+//    int a,b,c,d,e;
+//    cin >> a >> b >> c >> d >> e;
+//    cout << ((a>=0) || (b>=0)  || (c>=0) || (d>=0) || (e>=0)) << "\n";
+
+
+    // problem 20
+//    int a,b;
+//    cin >> a >> b;
+//    cout << (a+b>30 && a*b > 30) <<" ";
+//    cout <<(a+b-10>30 || a*(b-10) > 30) << " ";
+//    cout << (a>30 || b>30) << "\n";
+
+    return 0;
+}
